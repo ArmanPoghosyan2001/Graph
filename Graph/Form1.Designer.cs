435cdd532f13209f351e27e6ac96cbf5718337f3
09092021 <09092021@DESKTOP-I8RNFBJ>
2021-09-30 15:21:18 +0400

    Add project files.

---
+++ Graph/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Graph
+{
+    partial class Graphs
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sineBox = new System.Windows.Forms.PictureBox();
+            this.cosineBox = new System.Windows.Forms.PictureBox();
+            this.drawButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sineBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cosineBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sineBox
+            // 
+            this.sineBox.Location = new System.Drawing.Point(12, 12);
+            this.sineBox.Name = "sineBox";
+            this.sineBox.Size = new System.Drawing.Size(691, 244);
+            this.sineBox.TabIndex = 3;
+            this.sineBox.TabStop = false;
+            this.sineBox.Paint += new System.Windows.Forms.PaintEventHandler(this.sineBox_Paint);
+            // 
+            // cosineBox
+            // 
+            this.cosineBox.Location = new System.Drawing.Point(12, 278);
+            this.cosineBox.Name = "cosineBox";
+            this.cosineBox.Size = new System.Drawing.Size(691, 306);
+            this.cosineBox.TabIndex = 3;
+            this.cosineBox.TabStop = false;
+            this.cosineBox.Paint += new System.Windows.Forms.PaintEventHandler(this.cosineBox_Paint);
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(818, 42);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(93, 27);
+            this.drawButton.TabIndex = 4;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // Graphs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 596);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.cosineBox);
+            this.Controls.Add(this.sineBox);
+            this.Name = "Graphs";
+            this.Text = "Sine/Cosine graphs";
+            ((System.ComponentModel.ISupportInitialize)(this.sineBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cosineBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox sineBox;
+        private System.Windows.Forms.PictureBox cosineBox;
+        private System.Windows.Forms.Button drawButton;
+    }
+}
+
---
+++ Graph/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph
+{
+    public partial class Graphs : Form
+    {
+        Pen pen = new Pen(Color.Red, 2.0F);
+
+        List<PointF> sinePoints = new List<PointF> { };
+        List<PointF> cosinePoints = new List<PointF> { };
+
+        public Graphs()
+        {
+            InitializeComponent();
+        }
+
+        private double ConvertToRadians(int angle)
+        {
+            return (Math.PI * angle) / 180;
+        }
+
+        /// <summary>
+        /// GetSin method that gets angle as an argument 
+        /// </summary>
+        /// <param name="angle">angle</param>
+        /// <returns></returns>
+        private double GetSin(int angle)
+        {
+            double radians = ConvertToRadians(angle);
+            return Math.Sin(radians);
+        }
+
+        /// <summary>
+        /// GetCos method that gets angle as an argument
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <returns></returns>
+        private double GetCos(int angle)
+        {
+            double radians = ConvertToRadians(angle);
+            return Math.Cos(radians);
+        }
+
+        private void CalculatePoints(int range)
+        {
+            for (int i = 0; i < range; i++)
+            {
+                float sineY = (float)GetSin(i) * (-1);
+                float cosineY = (float)GetCos(i) * (-1);
+
+
+                float sineScale = sineBox.Height / 2;
+                float cosineScale = cosineBox.Height / 2;
+
+                sinePoints.Add(new PointF(i, sineY * sineScale + sineScale));
+                cosinePoints.Add(new PointF(i, cosineY * cosineScale + cosineScale));
+            }
+        }
+        private void InvalidateBoxes()
+        {
+            sineBox.Invalidate();
+            cosineBox.Invalidate();
+        }
+        private void sineBox_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+            if (sinePoints.Count()>0)
+            {
+                e.Graphics.DrawCurve(pen, sinePoints.ToArray());
+            }
+        }
+
+        private void cosineBox_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+            if (cosinePoints.Count() > 0)
+            {
+                e.Graphics.DrawCurve(pen, cosinePoints.ToArray());
+            }
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            CalculatePoints(1000);
+            InvalidateBoxes();
+        }
+    }
+}
